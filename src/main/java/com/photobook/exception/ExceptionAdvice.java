e4baebd0136649979ae07f0a1afd3d7443e74946
jsy3831 <jsy3831@naver.com>
2021-09-11 22:43:36 +0900

    slf4j 임시적용

--- src/main/java/com/photobook/exception/ExceptionAdvice.java
+++ src/main/java/com/photobook/exception/ExceptionAdvice.java
@@ -1,5 +1,6 @@
 package com.photobook.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,26 +8,31 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
 
+@Slf4j
 @RestControllerAdvice
 public class ExceptionAdvice {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("handleException throw Exception : {}", e.getMessage());
         return ErrorResponse.toResponseEntity(HttpStatus.BAD_REQUEST, e);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException e) {
+        log.error("handleConstraintViolationException throw ConstraintViolationException : {}", e.getMessage());
         return ErrorResponse.toResponseEntity(HttpStatus.BAD_REQUEST, e);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        log.error("handleIllegalArgumentException throw IllegalArgumentException : {}", e.getMessage());
         return ErrorResponse.toResponseEntity(HttpStatus.BAD_REQUEST, e);
     }
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
         return ErrorResponse.toResponseEntity(e.getErrorCode());
     }
 
